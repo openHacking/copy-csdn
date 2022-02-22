773a88e4f31f76b88f1645165d5248310b5a5157
openHacking <openHacking@126.com>
2022-02-22 17:08:51 +0800

    fix copy bug

--- content-script.js
+++ content-script.js
@@ -1,16 +1,18 @@
 ﻿(function () {
-  /*
-   *  DOM selector
-   *  @param selector {String} css selector for element
-   *  @param context {Element} root element
-   *  usage: $$('body')
-   *  result: You will get <body> element
-   */
-  function $$(selector, context) {
-    context = context || document;
-    var elements = context.querySelectorAll(selector);
-    return Array.prototype.slice.call(elements);
-  }
+/*
+ *  DOM selector
+ *  @param selector {String} css selector for element
+ *  @param context {Element} root element
+ *  usage: $$('body')
+ *  result: You will get <body> element
+ */
+function $$(selector, context) {
+  context = context || document;
+  var elements = context.querySelectorAll(selector);
+  return elements.length == 1
+    ? Array.prototype.slice.call(elements)[0]
+    : Array.prototype.slice.call(elements);
+}
 
   /**
    *
@@ -20,28 +22,38 @@
    * @param str
    * @param mimeType  'Text'
    */
-  function copy(str, mimeType = "text/plain") {
-    document.oncopy = function (event) {
-      event.clipboardData.setData(mimeType, str);
-      event.preventDefault();
-    };
-    document.execCommand("copy", false, null);
+  function copy(str) {
+    navigator.clipboard.writeText(str);
   }
 
-  $$(".hljs-button").forEach((btn) => {
-    // change title
-    btn.dataset.title = "复制";
-
-    btn.addEventListener("click", (e) => {
-      // copy code
-      copy(e.target.closest(".prism").innerText);
-
-      e.target.dataset.title = "复制成功";
-      setTimeout(() => {
-        e.target.dataset.title = "复制";
-      }, 1000);
-      e.stopPropagation();
-      e.preventDefault();
+    const buttons = Array.isArray($$(".hljs-button")) ? $$(".hljs-button") : [$$(".hljs-button")]
+  
+    buttons.forEach((btn) => {
+      // change title
+      btn.dataset.title = "复制";
+  
+      // remove click
+      btn.setAttribute('onclick','')
+      
+      elClone = btn.cloneNode(true);
+  
+      btn.parentNode.replaceChild(elClone, btn);
+      
+      elClone.addEventListener("click", (e) => {
+        // copy code
+        const parentPreBlock =  e.target.closest("pre")
+        const codeBlock = $$('code',parentPreBlock)
+        copy(codeBlock.innerText);
+  
+        e.target.dataset.title = "复制成功";
+        setTimeout(() => {
+          e.target.dataset.title = "复制";
+        }, 1000);
+        e.stopPropagation();
+        e.preventDefault();
+      });
+      
+  
     });
-  });
+
 })();
